b969a83fe0e32b6a4c7d5ee4fd4d2b20dd02ac8d
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2025-02-20 00:07:29 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    //here will call instance objects from the core classes
+    //will have to implement the core classes before calling them
+    //this is just a rough draft of the code
+}
