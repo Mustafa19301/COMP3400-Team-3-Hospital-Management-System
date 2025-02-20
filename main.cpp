57c3284278e68d960319189cef36049b8c6ca180
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2025-02-20 00:11:13 -0500

    updated some minor changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     //here will call instance objects from the core classes
     //will have to implement the core classes before calling them
-    //this is just a rough draft of the code
+    //this is just a rough draft of the code and more implementation will come soon...
 }
