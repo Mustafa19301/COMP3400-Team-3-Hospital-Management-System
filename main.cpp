699ef9f5586886da57629fd3d5e2ecad03fd64bd
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2025-02-20 00:39:13 -0500

    added small code of line text

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
+    std::cout << "Loading Hospital Management System..." << "\n";
+    std::cout << "Welcome to the Hospital Management System! Developed by Team 3" << "\n";
     //here will call instance objects from the core classes
     //will have to implement the core classes before calling them
     //this is just a rough draft of the code and more implementation will come soon...
